ea1a477f802d76ab6bf6c72f18581a97786e96dc
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-09-17 12:49:37 +0300

    Fix icons

--- src/components/CVForm/SectionHeader.js
+++ src/components/CVForm/SectionHeader.js
@@ -1,11 +1,28 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleChevronDown,
+  faCircleChevronUp,
+} from '@fortawesome/free-solid-svg-icons';
 import classes from './SectionHeader.module.css';
 
 const SectionHeader = ({ text, isShown, onToggleSection }) => {
   return (
     <div className={classes.header} onClick={onToggleSection}>
       <h3>{text}</h3>
-      {/* {!isShown && <ChevronDownIcon className={classes.toggle} />} */}
-      {/* {isShown && <ChevronUpIcon className={classes.toggle} />} */}
+      {!isShown && (
+        <FontAwesomeIcon
+          icon={faCircleChevronDown}
+          className="toggle"
+          size="lg"
+        />
+      )}
+      {isShown && (
+        <FontAwesomeIcon
+          icon={faCircleChevronUp}
+          className="toggle"
+          size="lg"
+        />
+      )}
     </div>
   );
 };
--- src/components/CVPreview/Skills.js
+++ src/components/CVPreview/Skills.js
@@ -1,28 +1,30 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as EmptyStar } from '@fortawesome/free-regular-svg-icons';
 import classes from './Skills.module.css';
 
-//TODO: icons
 const Skills = ({ skills }) => {
   const rating = skills.map((skill) => {
     return (
       <li className={classes.rating} key={skill.id}>
         <p>{skill.title}</p>
-        {/* <div>{getStars(skill.rating)}</div> */}
+        <div>{getStars(skill.rating)}</div>
       </li>
     );
   });
 
-  // function getStars(rating) {
-  //   const stars = [];
-  //   for (let i = 1; i <= 10; i++) {
-  //     if (i <= rating) {
-  //       stars.push(<StarIcon className={classes.icon} key={i} />);
-  //     } else {
-  //       stars.push(<EmptyStar className={classes.icon} key={i} />);
-  //     }
-  //   }
+  function getStars(rating) {
+    const stars = [];
+    for (let i = 1; i <= 10; i++) {
+      if (i <= rating) {
+        stars.push(<FontAwesomeIcon icon={faStar} key={i} size="sm" />);
+      } else {
+        stars.push(<FontAwesomeIcon icon={EmptyStar} key={i} size="sm" />);
+      }
+    }
 
-  //   return stars;
-  // }
+    return stars;
+  }
 
   return (
     <div>
